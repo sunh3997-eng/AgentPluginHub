f86ce3c966e10c63e99f77cf623bb0d66aa54f22
sunh3997-eng <sunh3997@gmail.com>
2026-04-03 01:43:45 -0700

    chore: add .next to gitignore, remove build artifacts

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/test/.openclaw/workspace/auto-projects/AgentPluginHub/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"rx2GolnIM98YC1rd6xAJK81xxE/Yxbe3Wjxzrt/Yhok=\"}"
